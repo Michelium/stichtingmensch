415712f142d3affecfb66255e77dc43a46bfc219
Michelium <michelhamelink@gmail.com>
2021-06-29 22:55:06 +0200

    added hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,37 @@
 <template>
-  <section>
-
-  </section>
+    <section id="hero">
+        <div class="row mx-0">
+            <div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 text-center text-section">
+                <h1>Men<span class="text-red">s</span>ch</h1>
+                <h2>Maatschappelijk en <wbr>sociale bijdrage</h2>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-  name: 'Hero',
+    name: 'Hero',
 }
 </script>
 
 <style scoped>
+section {
+    background-image: url("~@/assets/images/hero/hero.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    background-color: black;
+}
+
+h2 {
+    text-transform: unset;
+    margin-bottom: unset;
+}
+
+.text-section {
+    white-space: nowrap;
+    margin-top: 200px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container-fluid">
-    <Hero/>
-  </div>
+    <div class="container-fluid p-0">
+        <Hero/>
+    </div>
+    <div class="row mx-0">
+        <div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 import Hero from "../components/Hero";
 
 export default {
-  name: 'Home',
-  components: {
-      Hero,
-  }
+    name: 'Home',
+    components: {
+        Hero,
+    }
 }
 </script>
